0a3e772c9b13ba7d12b60a17c9a4964073b41903
jacobprice <j@cobprice.com>
2024-08-01 14:46:05 -0400

    Make the context work with hierarchy

--- src/ContextProvider.php
+++ src/ContextProvider.php
@@ -5,46 +5,66 @@ namespace Og\TemplateLoader;
 use Timber\Timber;
 use Og\TemplateLoader\ContextProviderInterface;
 
-class ContextProvider implements ContextProviderInterface {
-    public function __construct(private array $config = []) {}
+class ContextProvider implements ContextProviderInterface
+{
+    public function __construct(private array $config = [])
+    {
+    }
 
 
-    public function get_context(): array {
-        $context = Timber::context();
-        if(!isset($this->config['context_dir'])) {
-            return $context;
+    public function get_context(): array
+    {
+
+        if (!isset($this->config['context_dir'])) {
+            return Timber::context();
         }
+
         $context_dir = $this->config['context_dir'];
 
         $context_files = new \FilesystemIterator($context_dir);
-        $global_context = Timber::context();
-        foreach ($context_files as $file) {
-            $context = include $file->getPathname();
-            if(!is_array($context)) {
-                continue;
-            }
-            $global_context = array_merge($global_context, $context);
+        $templates = (Hierarchy::instance())->templates() ?? [];
+
+        $templates = array_map(function ($template) {
+            return $this->format_for_class_name($template);
+        }, $templates);
+
+        $mapped_context = [];
+
+        foreach($context_files as $file) {
+            $file_name = $file->getBasename('.php');
+            $mapped_context[$file_name] = $file->getPathname();
         }
-        return $global_context;
+
+        foreach($templates as $template) {
+            if(array_key_exists($template, $mapped_context)) {
+                $context = include $mapped_context[$template];
+                if(!is_array($context)) {
+                    continue;
+                }
+                return array_merge(Timber::context(), $context);
+            }
+        }
+
+        return Timber::context();
     }
 
 
     /**
-	 * format_for_class_name
-	 *
-	 * Converts a template name to a class name
-	 * example: page-home -> PageHome
+     * format_for_class_name
+     *
+     * Converts a template name to a class name
+     * example: page-home -> PageHome
      * example: archive-tribe_events -> ArchiveTribeEvents
-	 *
-	 * @param  string $template
-	 * @return string
-	 */
-	public function format_for_class_name($template)
-	{
+     *
+     * @param  string $template
+     * @return string
+     */
+    public function format_for_class_name($template)
+    {
         $template = str_replace('_', '-', $template);
-		$parts = explode('-', $template);
-		$parts = array_map('ucfirst', $parts);
-		return implode('', $parts);
-	}
+        $parts = explode('-', $template);
+        $parts = array_map('ucfirst', $parts);
+        return implode('', $parts);
+    }
 
 }
